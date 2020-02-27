bb49f52a01ee933209ed4c44805e8f71456b8036
Vinicius de Moraes <vinicoder@gmail.com>
2020-02-26 21:50:52 -0300

    fix: deprecated auth method changed to new method

--- src/api.js
+++ src/api.js
@@ -1,16 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.github.com'
-});
-
-api.interceptors.request.use(config => {
-  config.params = {
-    client_id: '34dbf7f8b7468aa19f71',
-    client_secret: 'a4d1bcaab4ef676e0c0dfa75c43f42bdcad8533d',
-    ...config.params
-  };
-  return config;
+  baseURL: 'https://api.github.com',
+  auth: {
+    username: '34dbf7f8b7468aa19f71',
+    password: 'a4d1bcaab4ef676e0c0dfa75c43f42bdcad8533d'
+  }
 });
 
 export default api;
