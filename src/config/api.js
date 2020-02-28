5061a02baccdb519108fbb404487093ac0054ebe
Vinicius de Moraes <vinicoder@gmail.com>
2020-02-27 23:54:23 -0300

    feature: cache plugin to axios

--- src/config/api.js
+++ src/config/api.js
@@ -1,10 +1,13 @@
-import axios from 'axios';
+import { setup } from 'axios-cache-adapter';
 
-const api = axios.create({
+const api = setup({
   baseURL: 'https://api.github.com',
   auth: {
     username: '34dbf7f8b7468aa19f71',
     password: 'a4d1bcaab4ef676e0c0dfa75c43f42bdcad8533d'
+  },
+  cache: {
+    maxAge: 15 * 60 * 1000
   }
 });
 
